1da90a3ed6c2dec6137e0696f0d583b3a9bba9f3
Arka <arkadiptamojumder14@gmail.com>
2024-09-12 00:49:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://pointo-internship-task-backend.onrender.com',
+      '/api': 'http://localhost:3000',
     },
   },
   plugins: [react()],
