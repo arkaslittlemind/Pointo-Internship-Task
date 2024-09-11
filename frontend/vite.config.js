12958e4907680c9181a77f7c2408d9b92e44b203
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2024-09-12 01:37:04 +0530

    Refactor proxy configuration in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      // '/api': 'http://localhost:3000',
-      '/api': 'https://pointo-internship-task-backend.vercel.app/',
+      '/api': 'http://localhost:3000',
     },
   },
   plugins: [react()],
